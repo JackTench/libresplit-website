aa10213809dc79086308aa03d37974ea4cebd43c
Jack Tench <79285604+JackTench@users.noreply.github.com>
2025-11-02 22:41:42 +0000

    Make loading bar a matching blue with the rest of the site theme

--- src/components/libresplit/AppLoading.tsx
+++ src/components/libresplit/AppLoading.tsx
@@ -3,7 +3,7 @@ import { BarLoader } from "react-spinners";
 export function AppLoading() {
   return (
     <div className="flex h-screen w-screen items-center justify-center">
-      <BarLoader color={"#00ff00"} />
+      <BarLoader color={"#2196f3"} />
     </div>
   );
 }
