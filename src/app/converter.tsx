cf83c96cda171c62ebfb034010fbc0b97908fc20
Jack Tench <79285604+JackTench@users.noreply.github.com>
2025-11-02 20:52:05 +0000

    Remove current converter page design

--- src/app/converter.tsx
+++ src/app/converter.tsx
@@ -49,43 +49,8 @@ export function Converter() {
   };
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-linear-to-tr from-gray-700 to-sky-900 p-6">
-      <div className="w-full max-w-lg space-y-6 rounded-lg bg-gray-800 p-6 shadow-lg">
-        <h1 className="text-center text-2xl font-bold text-white">
-          LibreSplit Converter
-        </h1>
-        <div className="space-y-4">
-          <input
-            type="file"
-            accept=".lss"
-            onChange={handleFileChange}
-            className="block w-full rounded-md border border-white px-3 py-2 text-white focus:ring focus:ring-indigo-500 focus:outline-none"
-          />
-          <button
-            onClick={handleSubmit}
-            disabled={!selectedFile}
-            className={
-              '${selectedFile ? "bg-indigo-600 hover:bg-indigo-700" : "bg-gray-400 cursor-not-allowed"} w-full rounded-md px-4 py-2 font-semibold text-white'
-            }
-          >
-            Convert
-          </button>
-        </div>
-        {result && (
-          <div className="space-y-4">
-            <p className="text-center font-medium text-green-600">
-              Conversion successful! Click the button below to download your
-              LibreSplit file.
-            </p>
-            <button
-              onClick={handleDownload}
-              className="w-full rounded-md bg-green-600 px-4 py-2 font-semibold text-white hover:bg-green-700"
-            >
-              Download
-            </button>
-          </div>
-        )}
-      </div>
+    <div>
+      <p>Placeholder</p>
     </div>
   );
 }
