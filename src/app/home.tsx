7bc4a8e9be4a38524a442e5799f56c0306d2cebf
Jack Tench <79285604+JackTench@users.noreply.github.com>
2025-11-03 17:55:02 +0000

    Fix centering on main page

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -3,7 +3,7 @@ import { AppHero } from "@/components/libresplit/AppHero";
 
 export function Home() {
   return (
-    <div className="flex w-screen flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center">
       <AppHero />
       <AppGitHubReadme />
     </div>
