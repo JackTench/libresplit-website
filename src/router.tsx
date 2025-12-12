17ec3ae28d61388d8e4438f27451b0e0f1141f91
JackTench <79285604+JackTench@users.noreply.github.com>
2025-12-12 11:27:42 +0000

    Implement auto splitters docs page

--- src/router.tsx
+++ src/router.tsx
@@ -1,3 +1,4 @@
+import { AutoSplitters } from "./app/auto-splitters";
 import { Converter } from "./app/converter";
 import { Home } from "./app/home";
 import { NotFound } from "./app/not-found";
@@ -8,6 +9,9 @@ export default function AppRouter() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/converter" element={<Converter />} />
+
+      {/* Documentation pages pulled from GitHub. */}
+      <Route path="/docs/auto-splitters.md" element={<AutoSplitters />} />
 
       {/* Fall back on app's 404 page. This is because of the SPA routing trick with 404.html used in GitHub Pages. */}
       <Route path="*" element={<NotFound />} />
