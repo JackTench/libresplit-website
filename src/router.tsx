45c70e531cd4b3c0bd5f40d19b681c34b75273d8
JackTench <79285604+JackTench@users.noreply.github.com>
2025-12-12 11:30:03 +0000

    Implement settings and keybinds docs page

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,7 @@ import { AutoSplitters } from "./app/auto-splitters";
 import { Converter } from "./app/converter";
 import { Home } from "./app/home";
 import { NotFound } from "./app/not-found";
+import { SettingsKeybinds } from "./app/settings-keybinds";
 import { Route, Routes } from "react-router";
 
 export default function AppRouter() {
@@ -12,6 +13,7 @@ export default function AppRouter() {
 
       {/* Documentation pages pulled from GitHub. */}
       <Route path="/docs/auto-splitters.md" element={<AutoSplitters />} />
+      <Route path="/docs/settings-keybinds.md" element={<SettingsKeybinds />} />
 
       {/* Fall back on app's 404 page. This is because of the SPA routing trick with 404.html used in GitHub Pages. */}
       <Route path="*" element={<NotFound />} />
