61ae9307fd50ba028548ca99c1650a824fd31a2f
JackTench <79285604+JackTench@users.noreply.github.com>
2025-12-12 11:34:37 +0000

    Implement troubleshooting page

--- src/router.tsx
+++ src/router.tsx
@@ -5,6 +5,7 @@ import { NotFound } from "./app/not-found";
 import { SettingsKeybinds } from "./app/settings-keybinds";
 import { SplitFiles } from "./app/split-files";
 import { Themes } from "./app/themes";
+import { Troubleshooting } from "./app/troubleshooting";
 import { Route, Routes } from "react-router";
 
 export default function AppRouter() {
@@ -18,6 +19,7 @@ export default function AppRouter() {
       <Route path="/docs/settings-keybinds.md" element={<SettingsKeybinds />} />
       <Route path="/docs/split-files.md" element={<SplitFiles />} />
       <Route path="/docs/themes.md" element={<Themes />} />
+      <Route path="/docs/troubleshooting.md" element={<Troubleshooting />} />
 
       {/* Fall back on app's 404 page. This is because of the SPA routing trick with 404.html used in GitHub Pages. */}
       <Route path="*" element={<NotFound />} />
