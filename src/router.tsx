66b3c6bad313e7d66fb7c53d0419230ebd720f13
JackTench <79285604+JackTench@users.noreply.github.com>
2025-12-12 11:31:40 +0000

    Implement split file docs page

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import { Converter } from "./app/converter";
 import { Home } from "./app/home";
 import { NotFound } from "./app/not-found";
 import { SettingsKeybinds } from "./app/settings-keybinds";
+import { SplitFiles } from "./app/split-files";
 import { Route, Routes } from "react-router";
 
 export default function AppRouter() {
@@ -14,6 +15,7 @@ export default function AppRouter() {
       {/* Documentation pages pulled from GitHub. */}
       <Route path="/docs/auto-splitters.md" element={<AutoSplitters />} />
       <Route path="/docs/settings-keybinds.md" element={<SettingsKeybinds />} />
+      <Route path="/docs/split-files.md" element={<SplitFiles />} />
 
       {/* Fall back on app's 404 page. This is because of the SPA routing trick with 404.html used in GitHub Pages. */}
       <Route path="*" element={<NotFound />} />
