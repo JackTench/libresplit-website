e36afb07b7e8caaf493599ac90fb44c9ccf4cd04
JackTench <79285604+JackTench@users.noreply.github.com>
2025-12-12 11:32:58 +0000

    Implement themes docs page

--- src/router.tsx
+++ src/router.tsx
@@ -4,6 +4,7 @@ import { Home } from "./app/home";
 import { NotFound } from "./app/not-found";
 import { SettingsKeybinds } from "./app/settings-keybinds";
 import { SplitFiles } from "./app/split-files";
+import { Themes } from "./app/themes";
 import { Route, Routes } from "react-router";
 
 export default function AppRouter() {
@@ -16,6 +17,7 @@ export default function AppRouter() {
       <Route path="/docs/auto-splitters.md" element={<AutoSplitters />} />
       <Route path="/docs/settings-keybinds.md" element={<SettingsKeybinds />} />
       <Route path="/docs/split-files.md" element={<SplitFiles />} />
+      <Route path="/docs/themes.md" element={<Themes />} />
 
       {/* Fall back on app's 404 page. This is because of the SPA routing trick with 404.html used in GitHub Pages. */}
       <Route path="*" element={<NotFound />} />
