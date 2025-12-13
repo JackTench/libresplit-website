c8b674f393619cdd14d4b2676fcfd544fa870d6c
JackTench <79285604+JackTench@users.noreply.github.com>
2025-12-13 02:06:47 +0000

    Move docs pages into their own folder

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,11 @@
-import { AutoSplitters } from "./app/auto-splitters";
 import { Converter } from "./app/converter";
+import { AutoSplitters } from "./app/docs/auto-splitters";
+import { SettingsKeybinds } from "./app/docs/settings-keybinds";
+import { SplitFiles } from "./app/docs/split-files";
+import { Themes } from "./app/docs/themes";
+import { Troubleshooting } from "./app/docs/troubleshooting";
 import { Home } from "./app/home";
 import { NotFound } from "./app/not-found";
-import { SettingsKeybinds } from "./app/settings-keybinds";
-import { SplitFiles } from "./app/split-files";
-import { Themes } from "./app/themes";
-import { Troubleshooting } from "./app/troubleshooting";
 import { Route, Routes } from "react-router";
 
 export default function AppRouter() {
